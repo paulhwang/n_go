52e1be08580bf8fc26c39d6059b067571a343a1d
phwang <phwang@gmail.com>
2022-01-06 12:41:57 +0800

    http_root

--- phwang.js
+++ phwang.js
@@ -1,7 +1,7 @@
 var net = require("net");
 var express = require('express');
 var bodyParser = require('body-parser');
-require('./phwang_modules/fabric_modules/fabric_root.js').malloc();
+require('./phwang_modules/fabric_modules/http_root.js').malloc();
 var http_input_module = require('./phwang_modules/fabric_modules/http_input.js');
 var app = express();
 
