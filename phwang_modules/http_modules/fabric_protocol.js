79568a33112bf57dfbd44945fe66e2ae12a8badf
phwang <phwang@gmail.com>
2022-01-07 08:40:16 +0800

    protocol

--- phwang_modules/http_modules/fabric_protocol.js
+++ phwang_modules/http_modules/fabric_protocol.js
@@ -4,10 +4,10 @@
  * File name: fabric_protocol.js
  */
 
-var FABRIC_SERVER_IP_ADDRESS = "127.0.0.1";
-var FABRIC_SERVER_TCP_PORT = 8006;
-var FABRIC_SERVER_AJAX_ID_SIZE = 3;
-var FABRIC_SERVER_DATA_LENGTH_SIZE = 5;
+var FABRIC_PROTOCOL_SERVER_IP_ADDRESS = "127.0.0.1";
+var FABRIC_PROTOCOL_SERVER_TCP_PORT = 8006;
+var FABRIC_PROTOCOL_AJAX_ID_SIZE = 3;
+var FABRIC_PROTOCOL_DATA_LENGTH_SIZE = 5;
 
 var the_fabric_protocol_object = null;
 
@@ -21,8 +21,8 @@ module.exports = {
 };
 
 function FabricProtocolClass () {
-    this.fabricSeriverIpAddr = function () {return FABRIC_SERVER_IP_ADDRESS;};
-    this.fabricSeriverTcpPort = function () {return FABRIC_SERVER_TCP_PORT;};
-    this.fabricSeriverAjaxIdSize = function () {return FABRIC_SERVER_AJAX_ID_SIZE;};
-    this.fabricSeriverDataLengthSize = function () {return FABRIC_SERVER_DATA_LENGTH_SIZE;};
+    this.fabricSeriverIpAddr = function () {return FABRIC_PROTOCOL_SERVER_IP_ADDRESS;};
+    this.fabricSeriverTcpPort = function () {return FABRIC_PROTOCOL_SERVER_TCP_PORT;};
+    this.fabricSeriverAjaxIdSize = function () {return FABRIC_PROTOCOL_AJAX_ID_SIZE;};
+    this.fabricSeriverDataLengthSize = function () {return FABRIC_PROTOCOL_DATA_LENGTH_SIZE;};
 }
