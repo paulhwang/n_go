453b2f315b60958a3a38363f4b2e1cfa92c61ed9
Paul Hwang <phwang@gmail.com>
2022-09-13 20:55:57 -0700

    result

--- phwang_modules/http_modules/http_service.js
+++ phwang_modules/http_modules/http_service.js
@@ -169,14 +169,14 @@ function HttpServiceClass(root_object_val) {
     this.setupLinkResponse = function (this0, data_val, ajax_entry_object_val) {
         this0.setLinkUpdateInterval(this0.defaultLinkUpdateInterval());
 
-        var link_id = data_val.slice(0, 8);
-        var result = data_val.slice(8);
+        var result = data_val.slice(0, 2);
+        var link_id = data_val.slice(2);
 
         var output = JSON.stringify({
                         my_name: ajax_entry_object_val.my_name,
                         time_stamp: this.fabricServiceObject().timeStampString(),
-                        link_id: link_id,
                         result: result,
+                        link_id: link_id,
                         });
         this0.debug(true, "setupLinkResponse", "output=" + output);
         this0.httpInputObject().sendHttpResponse(ajax_entry_object_val.ajaxRequest(), ajax_entry_object_val.ajaxResponse(), output);
