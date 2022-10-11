37e573a15033b02f20a2d038f0e9bb9b1826b0a3
Paul Hwang <phwang@gmail.com>
2022-10-11 11:15:57 -0700

    ts

--- phwang_modules/front_end_modules/http_service.js
+++ phwang_modules/front_end_modules/http_service.js
@@ -206,8 +206,7 @@ function HttpServiceClass(root_object_val) {
 
 
         const output = JSON.stringify({
-                        time_stamp: this.fabricServiceObject().timeStampString(),
-                        data: data_val,
+                        data: data_val + this.fabricServiceObject().timeStampString(),
                         });
         console.log("HttpServiceClass.loginResponse() output=" + output);
         this0.httpInputObject().sendHttpResponse(ajax_entry_object_val.ajaxRequest(), ajax_entry_object_val.ajaxResponse(), output);
